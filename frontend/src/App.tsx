9d04fd4e39e8d1c32d1f317f1a79a27ba37dd4fa
cleilson <cleilsonrock@gmail.com>
2021-05-13 20:04:04 -0300

    Home, navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,7 @@
-import BarChart from "components/BarChart";
-
 import Routes from "Routes";
 
 function App() {
-  return (
-    <Routes></Routes>
-  );
+  return <Routes></Routes>;
 }
 
 export default App;
