37321b4dc9ea188d9dbedbbfd20b4a506e24b2a3
cleilson <cleilsonrock@gmail.com>
2021-05-14 20:14:51 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
-import Chart from "react-apexcharts"; 
+import { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
 import { SaleSum } from "types/sale";
 import { BASE_URL } from "utils/requests";
 
@@ -9,22 +10,24 @@ type ChartData = {
 }
 
 const DonutChart = () => {
-  //FORMA ERRADA
-  let chartData : ChartData = { labels: [], series: []};
-  //FORMA ERRADA
-  axios.get(`${BASE_URL}/sale/amount-by-seller`)
-    .then(response => {
-      const data = response.data as SaleSum[];
-      const myLabels = data.map(x => x.sellerName);
-      const mySeries = data.map(x => x.sum);
+  const [chartData, setchartData] = useState<ChartData>({ labels: [], series: [] });
 
-      chartData = { labels: myLabels, series: mySeries };
-      console.log(response.data);
-    })
+  //FORMA ERRADA
+  //let chartData : ChartData = { labels: [], series: []};
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sale/amount-by-seller`)
+      .then(response => {
+        const data = response.data as SaleSum[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
+
+        setchartData({ labels: myLabels, series: mySeries });
+      })
+  }, []);
 
   //const mockData = {
-    //series: [477138, 499928, 444867, 220426, 473088],
-   // labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+  //series: [477138, 499928, 444867, 220426, 473088],
+  // labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
   //}
 
   const options = {
@@ -34,8 +37,8 @@ const DonutChart = () => {
   }
 
   return (
-    <Chart 
-      options={{ ...options, labels: chartData.labels}}
+    <Chart
+      options={{ ...options, labels: chartData.labels }}
       series={chartData.series}
       type="donut"
       height="240"
